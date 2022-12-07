61ed874205c4c99e0f776248d488c8b990bfadd9
Fernando <fernando.rodrigues@compasso.com.br>
2022-12-07 17:31:28 -0300

    V1

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function openNav() {
+    document.getElementById("mySidepanel").style.width = "250px";
+  }
+  
+function closeNav() {
+    document.getElementById("mySidepanel").style.width = "0";
+}
+
+function saberMais() {
+   document.getElementById("saberMais").innerHTML = "Olha só, funcionou! :)"
+}
+
